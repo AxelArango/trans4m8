3e728dfe3efc97f68489c78cd9f6465739412831
Axel Arango <110843705+AxelArango@users.noreply.github.com>
2025-06-23 10:03:10 +0200

    documentation update

--- R/pam2points.R
+++ R/pam2points.R
@@ -5,12 +5,11 @@
 #'@param gridid option to return the id of the grid, useful for validate number of assamblages.
 #'@export
 pam2points<-function(PAM,phylo_names=F,gridid=F){
-  require(dplyr)
   coords<-as.data.frame(PAM$Presence_and_Absence_Matrix[,1:2])
   PAM1<-PAM$Presence_and_Absence_Matrix[,-c(1,2)]
   points<-data.frame()
   for(i in 1:length(coords$`Longitude(x)`)){
-    pointa<-data.frame(Presence=PAM1[i,]) %>% 
+    pointa<-data.frame(Presence=PAM1[i,]) %>%
       filter(Presence==1)
     if(gridid==T){points2<-data.frame(x=coords[i,1],y=coords[i,2],sp=row.names(pointa),id=i)}else{
       points2<-data.frame(lon=coords[i,1],lat=coords[i,2],sp=row.names(pointa))}
