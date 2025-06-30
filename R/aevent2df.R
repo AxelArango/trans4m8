f37e1085cf1d856d0f57ee083729920bae32bf2d
Axel Arango <110843705+AxelArango@users.noreply.github.com>
2025-06-30 10:10:11 +0200

    new function

---
+++ R/aevent2df.R
@@ -0,0 +1,27 @@
+#'Cladogenetic Event table to summary data frame
+#'Transfors the standard anagenetic event tables from BioGeoBEARS SBMs into a data.frame that includes the anagenetic event of interest across the total number of calculated maps.
+#'@param event_tables a list with anagenetic tables acquired through the stochastic mapping from BioGeoBEARS.
+#'@param type type of anagenetic event, NULL by default. This can be subset dispersal "d" and extinction "e".
+#'@returns a data.frame with the type of event, the time at which it happened in MYA, at which node it happen and the area in which it happened.
+#'@export
+aevent2df<-function(event_tables,type=NULL){
+  eventx<-data.frame()
+    for(i in 1:length(event_tables)){
+      eventn<-event_tables[[i]]
+      if(type=="d"){
+        eventm<-eventn%>%
+        filter(event_type==type) %>%
+        select(event_type,time_bp,node,dispersal_to) %>%
+        mutate(map=i)}else{
+          eventm<-eventn%>%
+            filter(event_type==type) %>%
+            select(event_type,time_bp,node,extirpation_from) %>%
+            mutate(map=i)
+        }
+      names(eventm)<-c("type","MYA","node","area")
+      eventx<-rbind(eventx,eventm)
+    }
+
+
+  return(eventx)
+}
--- R/event2df.R
+++ R/event2df.R
@@ -1,14 +1,20 @@
+#'Cladogenetic Event table to summary data frame
+#'Transfors the standard cladogenetic event tables from BioGeoBEARS SBMs into a dataframe that includes the cladogenetic event of interest across the total number of calculated maps.
+#'@param event_tables a list with cladogenetic tables acquired through the stochastic mapping from BioGeoBEARS.
+#'@param type type of cladogenetic event, NULL by default. This can be subset cladogenesis "subset (s)", vicariance "vicariance (v)", in situ speciation "sympatry (y)", and founder events "founder (j)".
+#'@returns a data.frame with the type of event, the time at which it happened in MYA, at which node it happen and the area in which it happened.
+#'@export
 event2df<-function(event_tables,type=NULL){
   eventx<-data.frame()
   if(type!="founder (j)"){
   for(i in 1:length(event_tables)){
     eventn<-event_tables[[i]]
-    eventm<-eventn%>% 
-      filter(clado_event_type==type) %>% 
-      select(clado_event_type,time_bp,node,clado_event_txt) %>% 
+    eventm<-eventn%>%
+      filter(clado_event_type==type) %>%
+      select(clado_event_type,time_bp,node,clado_event_txt) %>%
       mutate(map=i)
-    area<-data.frame(area=eventm$clado_event_txt) %>% 
-      as.tibble ()%>% 
+    area<-data.frame(area=eventm$clado_event_txt) %>%
+      as.tibble ()%>%
       separate(area,c("area",NA),"-")
     eventm2<-data.frame(eventm[,-4],area=area$area)
     names(eventm2)<-c("type","MYA","node","map_n","area")
@@ -19,17 +25,17 @@ event2df<-function(event_tables,type=NULL){
       eventsm3<-rbind(eventsm3,evs)
     }
     eventx<-rbind(eventx,eventsm3)
-      
+
   }
   }else{for(i in 1:length(event_tables)){
     eventn<-event_tables[[i]]
-    eventm<-eventn%>% 
-      filter(clado_event_type==type) %>% 
-      mutate(map=i) %>% 
+    eventm<-eventn%>%
+      filter(clado_event_type==type) %>%
+      mutate(map=i) %>%
       select(clado_event_type,time_bp,node,map,clado_dispersal_to)
-      
-    #area<-data.frame(area=eventm$clado_event_txt) %>% 
-      #as.tibble ()%>% 
+
+    #area<-data.frame(area=eventm$clado_event_txt) %>%
+      #as.tibble ()%>%
       #separate(area,c("area",NA),"-")
     #eventm2<-data.frame(eventm[,-4],area=area$area)
     names(eventm)<-c("type","MYA","node","map_n","area")
@@ -40,9 +46,9 @@ event2df<-function(event_tables,type=NULL){
       #eventsm3<-rbind(eventsm3,evs)
     }
     eventx<-rbind(eventx,eventm)
-    
+
   }
-    
+
   return(eventx)
 }
 
