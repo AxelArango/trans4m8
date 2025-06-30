91925045eaf0b53bcc17c54741a7e842b9818bfc
Axel Arango <110843705+AxelArango@users.noreply.github.com>
2025-06-30 08:11:31 +0200

    Create event2df.R

---
+++ R/event2df.R
@@ -0,0 +1,48 @@
+event2df<-function(event_tables,type=NULL){
+  eventx<-data.frame()
+  if(type!="founder (j)"){
+  for(i in 1:length(event_tables)){
+    eventn<-event_tables[[i]]
+    eventm<-eventn%>% 
+      filter(clado_event_type==type) %>% 
+      select(clado_event_type,time_bp,node,clado_event_txt) %>% 
+      mutate(map=i)
+    area<-data.frame(area=eventm$clado_event_txt) %>% 
+      as.tibble ()%>% 
+      separate(area,c("area",NA),"-")
+    eventm2<-data.frame(eventm[,-4],area=area$area)
+    names(eventm2)<-c("type","MYA","node","map_n","area")
+    eventsm3<-data.frame()
+    for(j in 1:length(eventm2$type)){
+      ars<-data.frame(areas=strsplit2(eventm2$area[j],""))
+      evs<-data.frame(eventm2[j,-5],areas=ars)
+      eventsm3<-rbind(eventsm3,evs)
+    }
+    eventx<-rbind(eventx,eventsm3)
+      
+  }
+  }else{for(i in 1:length(event_tables)){
+    eventn<-event_tables[[i]]
+    eventm<-eventn%>% 
+      filter(clado_event_type==type) %>% 
+      mutate(map=i) %>% 
+      select(clado_event_type,time_bp,node,map,clado_dispersal_to)
+      
+    #area<-data.frame(area=eventm$clado_event_txt) %>% 
+      #as.tibble ()%>% 
+      #separate(area,c("area",NA),"-")
+    #eventm2<-data.frame(eventm[,-4],area=area$area)
+    names(eventm)<-c("type","MYA","node","map_n","area")
+    #eventsm3<-data.frame()
+    #for(j in 1:length(eventm2$type)){
+      #ars<-data.frame(areas=strsplit2(eventm2$area[j],""))
+      #evs<-data.frame(eventm2[j,-5],areas=ars)
+      #eventsm3<-rbind(eventsm3,evs)
+    }
+    eventx<-rbind(eventx,eventm)
+    
+  }
+    
+  return(eventx)
+}
+
